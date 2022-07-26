0dbd3e8f7ccd9b4673b04cd27a8eb35d87d615be
birendra20 <birendragupta20@gmail.com>
2022-07-26 13:58:03 +0530

    api details

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "bdf5af415cmsh70406a4f741c506p1f07efjsnaaafd2ae55b3", // enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "ae9d91faad7a9b3d83bcb33851b4ef01"; // enter your key from openweather API
---
+++ src/components/search/Search.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { AsyncPaginate } from "react-select-async-paginate";
+import { geoApiOptions, GEO_API_URL } from "../../api";
+
+const Search = (onSearchChange) => {
+  const [search, setSearch] = useState(null);
+
+  const loadOptions = (inputValue) => {};
+
+  const handleOnChange = (searchData) => {
+    setSearch(searchData);
+    onSearchChange(searchData);
+  };
+
+  return (
+    <AsyncPaginate
+      placeholder="Search for city"
+      debounceTimeout={600}
+      value={search}
+      onChange={handleOnChange}
+      loadOptions={loadOptions}
+    />
+  );
+};
+
+export default Search;
